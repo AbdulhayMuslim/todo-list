0a6b132fd7062ad9467202f74d985f875d999943
Abdulhay Muslim <AbdulhayMuslim@users.noreply.github.com>
2026-02-13 21:47:33 +0430

    js file updated in some codes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
-const addBtn = document.getElementById('#addbtn');
+const addBtn = document.getElementById("#addbtn");
 const rmBtn = document.getElementById('#rmbtn');
-const noteCard = document.getElementsByClassName('.notes');
 
-addBtn.addEventListener("click", addnote);
+addBtn.addEventListener('click', addnote);
 function addnote() {
-    let newNote = prompt('Add your new note:')
-
-    let note = document.createElement('div')
-
-    newNote.
+    alert("hello")
 }
