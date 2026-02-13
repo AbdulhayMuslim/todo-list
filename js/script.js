3ee025172efa27f73db7097e5fae8f540164c5cd
Abdulhay Muslim <AbdulhayMuslim@users.noreply.github.com>
2026-02-13 21:40:15 +0430

    working on js file of todo list app

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-const addBtn = document.querySelector('#addbtn');
-const rmBtn = document.querySelector('#rmbtn');
-const noteCard = document.querySelector('.notes');
+const addBtn = document.getElementById('#addbtn');
+const rmBtn = document.getElementById('#rmbtn');
+const noteCard = document.getElementsByClassName('.notes');
+
+addBtn.addEventListener("click", addnote);
+function addnote() {
+    let newNote = prompt('Add your new note:')
+
+    let note = document.createElement('div')
+
+    newNote.
+}
