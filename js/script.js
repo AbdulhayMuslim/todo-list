6004f305ab4daa9bced2c3ed76a388a33661801c
Abdulhay Muslim <AbdulhayMuslim@users.noreply.github.com>
2026-02-18 17:00:08 +0430

    array created to store new notes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 const addBtn = document.getElementById("addbtn");
 const rmBtn = document.getElementById('rmbtn');
 
+var notes = [''];
+
+
+
 addBtn.addEventListener('click', addnote);
+
 function addnote() {
-    document.createElement('div')
-}
+    let newN = prompt('Write your note:');
+
+    newN.push(notes);
+    };
+
+    console.log(notes);
