9231428d233bc2e35cc39daa8c871cc5f3b3396a
Abdulhay Muslim <AbdulhayMuslim@users.noreply.github.com>
2026-02-18 17:03:06 +0430

    js code of todo list refined

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@ const rmBtn = document.getElementById('rmbtn');
 
 var notes = [''];
 
-
-
 addBtn.addEventListener('click', addnote);
 
 function addnote() {
     let newN = prompt('Write your note:');
 
     newN.push(notes);
-    };
+};
 
-    console.log(notes);
+console.log(notes);
